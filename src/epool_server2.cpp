bff81e963ff7b607ba824a08e862766c1e5d1862
andychow01 <andychow01@163.com>
2014-08-29 00:19:23 +0800

    xxxx

--- src/epool_server2.cpp
+++ src/epool_server2.cpp
@@ -1,12 +1,15 @@
 
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
 #include <sys/socket.h>
 #include <sys/epoll.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <errno.h>
 
 using namespace std;
