7ce9432161b7984fdf38bece181f4addec3f24ae
andychow01 <andychow01@163.com>
2015-06-06 08:34:40 -0700

    delete some space from origin

--- src/main_epollserver.cpp
+++ src/main_epollserver.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main()
 {
-        CEpollServer  theApp;
-        theApp.InitServer("127.0.0.1", 8000);
-        theApp.Run();
+    CEpollServer  theApp;
+    theApp.InitServer("127.0.0.1", 8000);
+    theApp.Run();
 
-        return 0;
+    return 0;
 }
